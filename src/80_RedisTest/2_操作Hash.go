585854ba8caa51537f967d93cbfa5b566eff83a8
kanned1079 <58361983+kanned1079@users.noreply.github.com>
2024-04-30 22:46:13 +0800

    update0430

---
+++ src/80_RedisTest/2_操作Hash.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+)
+
+var ipaddr string = "redis.orb.local"
+var port string = "6379"
+
+func main() {
+	// 1.新建连接
+	conn, err := redis.Dial("tcp", fmt.Sprintf("%s:%s", ipaddr, port))
+	if err != nil {
+		fmt.Println("连接到Redis失败", err)
+		return
+	} else {
+		fmt.Println("连接到Redis成功")
+	}
+	defer conn.Close()
+
+	_, err = conn.Do("SELECT", "1")
+
+	// 2.向Redis写入key-value数据
+	_, err = conn.Do("HSET", "user1", "name", "kanna")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("写入数据成功")
+	}
+	_, err = conn.Do("HSET", "user1", "age", "18")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("写入数据成功")
+	}
+
+	// 2.向Redis写入key-value数据
+	//r, err := conn.Do("GET", "key1")
+	r, err := redis.String(conn.Do("HGET", "user1", "name"))
+	// 这里返回的r是空接口类型
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("读取数据成功")
+	}
+	// 因此需要进行转换
+	//r1 := reflect.ValueOf(r).
+	// 不需要自己进行转换 Redis提供了很多方法
+	fmt.Println(r)
+
+	e, err := redis.Int(conn.Do("HGET", "user1", "age"))
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("读取数据成功")
+	}
+
+	fmt.Println(e)
+	fmt.Printf("Type(e) = %T\n", e)
+}

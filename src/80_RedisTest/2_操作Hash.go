2bfb88b96d909e9af35c12ce3c8a194396e8c034
kanna <kanna@kamiizumi.onmicrosoft.com>
2024-05-02 16:59:10 +0800

    update0502

--- src/80_RedisTest/2_操作Hash.go
+++ src/80_RedisTest/2_操作Hash.go
@@ -5,8 +5,8 @@ import (
 	"github.com/gomodule/redigo/redis"
 )
 
-var ipaddr string = "redis.orb.local"
-var port string = "6379"
+var ipaddr string = "api.ikanned.com"
+var port string = "16379"
 
 func main() {
 	// 1.新建连接
@@ -20,6 +20,7 @@ func main() {
 	defer conn.Close()
 
 	_, err = conn.Do("SELECT", "1")
+	// 在这里选择了第二个数据库
 
 	// 2.向Redis写入key-value数据
 	_, err = conn.Do("HSET", "user1", "name", "kanna")
@@ -59,4 +60,20 @@ func main() {
 
 	fmt.Println(e)
 	fmt.Printf("Type(e) = %T\n", e)
+
+	// 上面都是一个个的放
+	// 下面是一次放多个
+	_, err = conn.Do("HMset", "name", "张三", "age", 40)
+	// 同样的也可以多个读取
+	rr, err := redis.Strings(conn.Do("Mget", "name", "age"))
+	fmt.Printf("rr.Type() = %T\n", rr) // 这里的数据类型是 []string
+	fmt.Println(rr)
+	for i := range rr {
+		fmt.Println(rr[i])
+	}
+
+	_, _ = conn.Do("SET", "UserName", "kinggyo")
+
+	// 这里设置有效时间为60s
+	_, _ = conn.Do("EXPIRE", "UserName", 60)
 }

b339602af837e9b8c183ac5414182b25528d9e24
kanned1079 <58361983+kanned1079@users.noreply.github.com>
2024-05-03 23:07:43 +0800

    update0503

--- src/80_RedisTest/6_List作业.go
+++ src/80_RedisTest/6_List作业.go
@@ -29,12 +29,53 @@ func main() {
 	// 添加到List中
 	var classics = [4]string{"西游记", "红楼梦", "水浒传", "三国演艺"}
 	for i := 0; i < len(classics); i++ {
-		content := classics[i]
-		_, err = conn.Do("rPush", "classics", content)
+		_, err = conn.Do("rPush", "classics", classics[i])
 		if err != nil {
 			fmt.Println("rPush error", err)
 			panic(err)
 		}
 	}
 
+	read6(conn)
+
+	popL6(conn) // 第一次pop出 西游记
+	popL6(conn) // 第二次pop出 红楼梦
+	popR6(conn) // 这是从右侧pop出 三国演绎
+	//popR6(conn) // 再pop一次这个List就消失了
+
+}
+
+func read6(conn redis.Conn) {
+	var err error
+	fmt.Println("这是range")
+	data, err := redis.Strings(conn.Do("lRange", "classics", 0, -1))
+	if err != nil {
+		fmt.Println("lRange error", err)
+		panic(err)
+	}
+	for i := range data {
+		fmt.Println(i, data[i])
+	}
+}
+
+func popL6(conn redis.Conn) {
+	var err error
+	data, err := redis.String(conn.Do("lPop", "classics"))
+	// 注意这里不要用Strings 因为pop是一个个推出 得到的是一个而不是一组
+	if err != nil {
+		fmt.Println("lPop error", err)
+		panic(err)
+	}
+	fmt.Println("左侧pop = ", data)
+}
+
+func popR6(conn redis.Conn) {
+	var err error
+	data, err := redis.String(conn.Do("rPop", "classics"))
+	// 如上 为什么要用string
+	if err != nil {
+		fmt.Println("lPop error", err)
+		panic(err)
+	}
+	fmt.Println("右侧pop = ", data)
 }

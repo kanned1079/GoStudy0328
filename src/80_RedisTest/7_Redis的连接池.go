a14009e1a0a03bb5e479a725cf424c56095bfa1f
kanned1079 <58361983+kanned1079@users.noreply.github.com>
2024-05-04 00:11:22 +0800

    update0504

--- src/80_RedisTest/7_Redis的连接池.go
+++ src/80_RedisTest/7_Redis的连接池.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gomodule/redigo/redis"
+// Redis链接池
+// 通过Golang 对Redis操作 还可以通过Redis链接池
+// 流程如下
+// 1)事先初始化一定数量的链接 放入到链接池公
+// 2)当Go需要操作Redis时 直接从Redis链接池取出链接即可
+// 3)这样可以节省临时获取Redis链接的时间 从而提高效率.
 
 // 使用set
 // 存放商品信息
@@ -10,17 +15,19 @@ import "github.com/gomodule/redigo/redis"
 
 func main() {
 	//var err error
-	// 连接池
-	var pool *redis.Pool
-	pool = &redis.Pool{
-		MaxIdle:     8,
-		MaxActive:   0,
-		IdleTimeout: 100,
-		Dial: func() (redis.Conn, error) {
-			return redis.Dial("tcp", "api.ikanned.com:16379")
-		},
-	}
-	//c := pool.Get()
-	pool.Close()
+	// 连接池核心代码
+	/*
+		var pool *redis.Pool
+		pool = &redis.Pool{
+			MaxIdle:     8,
+			MaxActive:   0,
+			IdleTimeout: 100,
+			Dial: func() (redis.Conn, error) {
+				return redis.Dial("tcp", "api.ikanned.com:16379")
+			},
+		}
+		//c := pool.Get()
+		pool.Close()
+	*/
 
 }

f13717a899521da5d6915b85fb7bd036a361c432
kanned1079 <58361983+kanned1079@users.noreply.github.com>
2024-07-16 07:58:53 +0800

    update0716

--- Vue2/36_组件/2_单文件组件/vue_test1/src/router/index.js
+++ Vue2/36_组件/2_单文件组件/vue_test1/src/router/index.js
@@ -33,11 +33,11 @@ export default new VueRouter({
                             // props的第一种写法 值为对象 该对象中所有的key value都会以props的形式传递给Detail组件
                             // props: {a: 1, b: 'hellohello'}
                             // 第二种写法 如果是true 就会把该路由组件收到的所有params参数 以props的形式传给Detail
-                            // props: true,
+                            props: true,
                             // 第三种写法 值为函数
-                            props($route) {
-                                return {id: $route.query.id, title: $route.query.title};
-                            }
+                            // props($route) {
+                            //     return {id: $route.query.id, title: $route.query.title};
+                            // }
                         }
                     ]
                 },
